4c63eff6c0d9e0020df61bef1ae807ce2ba2e3a5
Maikel David Pérez Gómez <mdperez86@gmail.com>
2018-05-18 12:19:56 -0400

    Adding ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
+import { isPlatformBrowser } from '@angular/common';
+import { NgModule, Inject, PLATFORM_ID, APP_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'flight-status' })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string) {
+    const platform = isPlatformBrowser(platformId) ?
+      'in the browser' : 'on the server';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
