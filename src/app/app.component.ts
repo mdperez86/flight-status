92e98476cc5c032c78d18793560e5ec67031cb50
Maikel David Pérez Gómez <mdperez86@gmail.com>
2018-05-22 13:53:40 -0400

    Improve form structure and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 import { FlightStatusService } from './flight-status.service';
 
 @Component({
@@ -9,12 +11,13 @@ import { FlightStatusService } from './flight-status.service';
 })
 export class AppComponent implements OnInit {
 
+  isLoading = false;
   form: FormGroup;
   fligthStatus: any;
-  step = 1;
   minDate = new Date();
+  step = 1;
 
-  constructor(private service: FlightStatusService) {
+  constructor(private service: FlightStatusService, public snackBar: MatSnackBar) {
     this.form = new FormGroup({
       date: new FormControl(null, Validators.required),
       flight: new FormControl(null, Validators.required),
@@ -28,9 +31,14 @@ export class AppComponent implements OnInit {
   }
 
   onSubmit(): void {
+    this.isLoading = true;
     this.service.find(this.form.value.date, this.form.value.flight).subscribe(response => {
       this.fligthStatus = response;
+      this.isLoading = false;
       this.step = 3;
+    }, (error: Error) => {
+      this.snackBar.open(error.message, null, { duration: 5000 });
+      this.isLoading = false;
     });
   }
 }
