59a51bc5d808b20939515929c0f30de4260d0a2f
Maikel David Perez Gomez <mdperez86@gmail.com>
2018-06-08 12:34:36 -0400

    Change to e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display Flight Status as card title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getCardTitle()).toEqual('Flight Status');
   });
 });
